90b3bf322cf42a4fcc297a9682af6e8cce2a4cd2
DeepakR-28 <awsmyo1234@gmail.com>
2022-03-01 16:45:46 +0530

    updated image path

--- pages/api/photos/index.js
+++ pages/api/photos/index.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     var imagekit =  new ImageKit({
      publicKey : process.env.PUBLIC_IMAGEKIT_KEY,
      privateKey :process.env.PRIVATE_IMAGEKIT_KEY,
-     urlEndpoint : "https://ik.imagekit.io/6rhjv1amjqq/"
+     urlEndpoint : process.env.IMAGE_KIT_URL
  });
 
     const data =  await imagekit.listFiles({
