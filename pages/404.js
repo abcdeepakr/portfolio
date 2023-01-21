2acac0338d8ad81dd8cc527ec1661fe5589e892c
Deepak Rawat <awsmyo1234@gmail.com>
2023-01-21 09:35:20 +0530

    add 404 page

---
+++ pages/404.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import notFoundImage from '../public/images/notfound.jpg'
+import Button from 'react-bootstrap/Button';
+
+function fourOhFour() {
+    return (
+        <div style={{"display":"flex","justifyContent":"center","alignItems":"center", "flexDirection":"column"}}>
+            <Image
+                src={notFoundImage}
+                alt="Not found"
+                width={800}
+                height={400}
+            />
+            <b>404: Page not found</b> 
+        </div>
+        
+    )
+}
+
+export default fourOhFour
